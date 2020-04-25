0ceadbcaec9dd996f9e1fec6362f387e3f25c050
Djenifer R. P <djeniferrenata@yahoo.com.br>
2020-04-25 18:53:04 -0300

    Create pipeline to save item in a database

--- crawler/doramas_crawler/pipelines.py
+++ crawler/doramas_crawler/pipelines.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sqlite3
 
 # Define your item pipelines here
 #
@@ -7,5 +8,32 @@
 
 
 class DoramasCrawlerPipeline(object):
+    database = 'test.sqlite'
+    test = True
+
+    def open_spider(self, spider):
+        self.conn = sqlite3.connect(self.database)
+        self.cursor = self.conn.cursor()
+        if self.test:
+            self.cursor.execute('DROP TABLE IF EXISTS dorama')
+            self.cursor.execute('''
+                CREATE TABLE dorama (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    link TEXT NOT NULL,
+                    fansub TEXT NOT NULL
+                )'''
+            )
+            self.conn.commit()
+
+    def close_spider(self, spider):
+        self.conn.close()
+
     def process_item(self, item, spider):
+        values = (item['title'], item['link'], spider.name)
+        self.cursor.execute('''
+            INSERT INTO dorama (title, link, fansub)
+            VALUES (?, ?, ?)''', values)
+        self.conn.commit()
+
         return item
--- crawler/doramas_crawler/settings.py
+++ crawler/doramas_crawler/settings.py
@@ -64,9 +64,9 @@ DOWNLOAD_DELAY = 0.5
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'doramas_crawler.pipelines.DoramasCrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'doramas_crawler.pipelines.DoramasCrawlerPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
