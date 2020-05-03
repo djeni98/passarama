37e74185f8425f4a7a3fbfe1329081a2b5ac7e6d
Djenifer R. P <djeniferrenata@yahoo.com.br>
2020-05-02 21:42:29 -0300

    Implement api search request

--- front/src/components/Search/index.js
+++ front/src/components/Search/index.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 import { useHistory, useLocation } from 'react-router-dom';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
+import api from '../api';
 import SearchBar from '../SearchBar';
 import Footer from '../Footer';
 
@@ -11,9 +12,20 @@ import './styles.css';
 
 export default function SearchPage () {
   const [doramas, setDoramas] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [noResults, setNoResults] = useState(false);
 
   function getDorama(value) {
-    console.log('getDorama', value);
+    const params = value ? { title: value } : {}
+
+    api.get('doramas', { params })
+      .then(response => {
+        console.log(response.headers);
+        setTotal(response.headers['x-total-count']);
+        setDoramas(response.data);
+
+        setNoResults(!response.headers['x-total-count']);
+      })
   }
 
   const location = useLocation();
@@ -38,6 +50,39 @@ export default function SearchPage () {
           <Col />
         </Row>
       </Container>
+
+      { total ? (
+        <Container className="mb-5">
+          <Row className="mb-3">
+            <Col>
+              <h2 className="text-right">{total} Resultado(s)</h2>
+            </Col>
+          </Row>
+          <Row xs={1} md={2} lg={3} xl={4} className="align-items-stretch mb-3">
+            {doramas.map((dorama, index) => (
+                <Col key={index} className="p-15px">
+                  <Card className="orange-border h-100">
+                    <Card.Body className="d-flex flex-column justify-content-between">
+                      <Card.Link href={dorama.link} target="_blank">
+                        <Card.Title>{dorama.title}</Card.Title>
+                      </Card.Link>
+                      <Card.Text>{dorama.fansub}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))
+            }
+          </Row>
+        </Container>
+      ) : null }
+
+      { noResults ? (
+        <Container className="mb-5">
+          <Row className="justify-content-center">
+            <h1>Nenhum resultado foi encontrado</h1>
+          </Row>
+        </Container>
+      ) : null }
 
       <Footer />
     </>
