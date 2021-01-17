cbf850c020697efab70abd9d7cddb608fc69e8b6
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-01-17 12:58:25 -0300

    Create file to keep api/back version

--- back/setup.py
+++ back/setup.py
@@ -1,3 +1,7 @@
+from os.path import dirname, join
 from setuptools import setup, find_packages
 
-setup(name='passarama_api', version='0.1.0', packages=find_packages())
+with open(join(dirname(__file__), 'passarama_api/VERSION'), 'r') as f:
+    version = f.read().strip()
+
+setup(name='passarama_api', version=version, packages=find_packages())
