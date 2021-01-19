29244ff48cdae9ae741f6c44c18613eab6c38cfe
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-01-19 15:59:55 -0300

    Update About page test

--- front/src/components/About/About.test.js
+++ front/src/components/About/About.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import api from '../api';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import About from './index.js';
 
-test('renders about page', () => {
+import { mockGetObj as mockGet } from '../test_utils';
+
+jest.mock('../api');
+
+test('renders about page with info error', async () => {
+  mockGet({}, true);
+
   const { container } = render(<About />, { wrapper: MemoryRouter });
 
   const header = container.querySelector('header');
@@ -31,6 +38,38 @@ test('renders about page', () => {
   steps.forEach((step, index) => {
     expect(step).toHaveTextContent(expectedSteps[index]);
   });
+  
+  const infoTitle = screen.getByRole('heading', { name: /informaç(ão|ões)/i });
+  const infosDesc = screen.getAllByRole('definition');
+
+  expect(infoTitle).toBeInTheDocument();
+
+  let infosContent = ['-','-', 'github'];
+  infosDesc.forEach((info, index) =>
+    expect(info).toHaveTextContent(infosContent[index])
+  );
+
+  const unavailableInfo = await screen.findAllByText(/não foi possível/i);
+  
+  expect(unavailableInfo).toHaveLength(2);
 
   expect(footer).toBeInTheDocument();
+});
+
+test('renders info section with success', async () => {
+  const version = 'some version';
+  const update = '2222-11-22';
+  mockGet({ api_version: version, db_last_update: update });
+
+  render(<About />, { wrapper: MemoryRouter });
+
+  const [yyyy, mm, dd] = update.split('-');
+  const db_update = await screen.findByText(new RegExp(yyyy));
+  const api_version = await screen.findByText(new RegExp(version));
+  
+  expect(db_update).toHaveTextContent(yyyy);
+  expect(db_update).toHaveTextContent(mm);
+  expect(db_update).toHaveTextContent(dd);
+
+  expect(api_version).toBeInTheDocument();
 });
